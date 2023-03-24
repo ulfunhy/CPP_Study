d1d37de2d6b296f673fc9f39f52bc3b124e5ed82
LeeHyunJin <myfamily11141@gmail.com>
2023-03-24 09:44:44 +0900

    C스타일 문자열 길이

---
+++ 2023 CPP/문법/문자열 길이.cpp
@@ -0,0 +1,22 @@
+﻿// C
+#include <iostream>
+#include <string.h>
+
+// C++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	char str1[30] = "Jiwoo";
+	int len = 0;
+
+	for (int i = 0; str1[i] != '\0'; i++) {
+		len++;
+	}
+
+	printf("문자열의 길이는 %d \n", len);
+
+	return 0;
+}
