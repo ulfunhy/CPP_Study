4257f59f1cae744c4a5cc5b09975876aa22ad5a1
LeeHyunJin <myfamily11141@gmail.com>
2023-03-31 09:53:15 +0900

    class, 접근제어자 public

--- 2023 CPP/문법/클래스 기본.cpp
+++ 2023 CPP/문법/클래스 기본.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 // 구조체, 클래스는 일반적으로 단어의 첫글자를 대문자로 한다
-struct Student {
+// 클래스가 디폴트 private (struct는 디폴트가 public)
+class Student {
 	// 성능때문에 num을 문자열로 하지 않음
 	// 일반적으로 문자열은 정수형 데이텁다 많은 메모리 공간을 요구하며
 	// 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
+public:
 	int num;
 	string name;
 	string tel;
@@ -19,7 +21,7 @@ struct Student {
 int main(void) 
 {
 	// 자료형 : struct Student(C++에서는 pizza만 써도 가능
-	struct Student stu1;
+	Student stu1;
 	stu1.num = 2213;
 	stu1.name = "이현진";
 	stu1.tel = "010-8641-3308";
