35317decb32fa5c7a0addce3eddea05d9b38bb8d
LeeHyunJin <myfamily11141@gmail.com>
2023-04-18 14:41:55 +0900

    멤버변수 동적할당

--- 2023 CPP/문법/클래스 중급.cpp
+++ 2023 CPP/문법/클래스 중급.cpp
@@ -1,17 +1,22 @@
 ﻿#include <iostream>
+#include <string.h>
+
 using namespace std;
 
 class MString {
 public:
-	MString(const char* str) 
+	MString(const char* str)	// str은 "I will be back"의 주소값을 가리키는 중
 	{
-		c_str_ = str;
+		unsigned int l = strlen(str);
+		c_str_ = new char[l+1];			// '\0'(널문자)가 들어갈 공간 +1
+		strcpy(c_str_, str);		// c_str을 str로 복사
+		size_ = l;
 	}
 	unsigned int size(void) { return size_; }
-	const char* c_str(void) { return c_str_; }
+	char* c_str(void) { return c_str_; }
 private:
 	unsigned int size_;		// 문자열의 길이
-	const char* c_str_;		// 문자열의 시작주소
+	char* c_str_;		// 문자열의 시작주소
 };
 
 int main(void)
