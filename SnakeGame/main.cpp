32c8034b7d0b9087a40668c0cd7466e7f5037fa6
LeeHyunJin <myfamily11141@gmail.com>
2023-05-26 09:59:20 +0900

    [SFML] 사과와 뱀의 픽셀 맞추기 수정(전에 했던것) & 방향을 바꾸는 방식으로 전환

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,26 +1,33 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <stdlib.h>		// srand(), rand()
 #include <time.h>		// time()
+#include <stdio.h>
+
+#define DIR_UP		0
+#define DIR_DOWN	1
+#define DIR_RIGHT	2
+#define DIR_LEFT	3
 
 using namespace sf;
 
 int main(void)
 {
-	const int WIDTH = 1000;
-	const int HEIGHT = 800;
-	const int BLOCK_SIZE = 50;
-	const int G_WIDTH = WIDTH / BLOCK_SIZE;
-	const int G_HEIGHT = HEIGHT / BLOCK_SIZE;
+	const int WIDTH = 1000;						// 픽셀 너비
+	const int HEIGHT = 800;						// 픽셀 높이
+	const int BLOCK_SIZE = 50;					// 한 칸이 가지고 있는 픽셀
+	const int G_WIDTH = WIDTH / BLOCK_SIZE;		// 그리드 너비
+	const int G_HEIGHT = HEIGHT / BLOCK_SIZE;	// 그리드 높이
 
 	srand(time(NULL));
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Snake Game");	// 윈도우 창 만들기
 
 	// 컴퓨터가 1초동안 처리하는 횟수는 60으로 제한한다.
 	// Frame Per Second를 30으로 조절
-	window.setFramerateLimit(30);
+	window.setFramerateLimit(10);
 
 	RectangleShape snake;
 	int snake_x = 1, snake_y = 2;	// 뱀의 그리드 좌표
+	int snake_dir = DIR_DOWN;		// 뱀이 이도아는 방향
 	snake.setFillColor(Color::White);
 	snake.setPosition(snake_x * BLOCK_SIZE, snake_y * BLOCK_SIZE);
 	snake.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
@@ -29,7 +36,7 @@ int main(void)
 	int apple_x = rand() % G_WIDTH, apple_y = rand() % G_HEIGHT;
 	apple.setFillColor(Color::Red);
 	// 640에 50을 뺀 이유는 사과가 화면 밖에 벗어나지 않게 하기 위함
-	apple.setPosition(rand() % (apple_y * BLOCK_SIZE), rand() % (apple_y * BLOCK_SIZE));
+	apple.setPosition(rand() % (apple_x * BLOCK_SIZE), rand() % (apple_y * BLOCK_SIZE));
 	apple.setSize(Vector2f(BLOCK_SIZE, BLOCK_SIZE));
 
 	while (window.isOpen())
@@ -43,28 +50,40 @@ int main(void)
 		}
 		// 방향키가 동시에 눌러지지 않도록 else 처리(else commit에 실수로 미작성됨)
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			snake_x++;
-			snake.move(BLOCK_SIZE, 0);
+			snake_dir = DIR_RIGHT;
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			snake_x--;
-			snake.move(-BLOCK_SIZE, 0);
+			snake_dir = DIR_LEFT;
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Up)) {
-			snake_y--;
-			snake.move(0, -BLOCK_SIZE);
+			snake_dir = DIR_UP;
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-			snake_y++;
-			snake.move(0,BLOCK_SIZE);
+			snake_dir = DIR_DOWN;
 		}
+
+		// update
+		if (snake_dir == DIR_UP) {
+			snake_y--;
+		}
+		else if (snake_dir == DIR_DOWN) {
+			snake_y++;
+		}
+		else if (snake_dir == DIR_RIGHT) {
+			snake_x++;
+		}
+		else if (snake_dir == DIR_LEFT) {
+			snake_x--;
+		}		// 그리드 좌표
+		snake.setPosition(snake_x * BLOCK_SIZE, snake_y * BLOCK_SIZE);		// 픽셀 좌표
 
 		// 뱀이 사과를 먹었을 때, 
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds())) {
 			apple_x = rand() % G_WIDTH, apple_y = rand() % G_HEIGHT;
-			apple.setPosition(rand() % (WIDTH - BLOCK_SIZE), rand() % (HEIGHT - BLOCK_SIZE));
+			apple.setPosition(apple_x * BLOCK_SIZE, apple_y * BLOCK_SIZE);
 		}
 
+		// render
 		window.clear();
 
 		window.draw(snake);
