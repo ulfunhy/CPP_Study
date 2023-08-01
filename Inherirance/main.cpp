2a800ac57f76333043f57c5ad8f1abe7cb9436e9
LeeHyunJin <myfamily11141@gmail.com>
2023-08-01 14:57:08 +0900

    [상속] 적을 만들기

--- Inherirance/main.cpp
+++ Inherirance/main.cpp
@@ -1,4 +1,6 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace sf;
 
@@ -37,14 +39,24 @@ private:
 
 int main(void)
 {
+	srand((unsigned int)time(NULL));
+
 	RenderWindow window(VideoMode(1000, 800), "Sangsok");
+	window.setFramerateLimit(60);
 
 	RectangleShape p;
 	p.setFillColor(Color::White);
 	p.setPosition(100, 300);
 	p.setSize(Vector2f(50, 50));
 
+	RectangleShape e1;
+	e1.setFillColor(Color::Red);
+	e1.setPosition(rand()%800, rand()%600);
+	e1.setSize(Vector2f(40, 40));
+
 	Entity* player = new Entity(3, 5, &p);
+
+	Entity* enemy1 = new Entity(1, 3, &e1);
 
 
 	while (window.isOpen())
@@ -70,6 +82,7 @@ int main(void)
 
 		window.clear();
 
+		window.draw(enemy1->get_sprite());
 		window.draw(player->get_sprite());
 
 		window.display();
