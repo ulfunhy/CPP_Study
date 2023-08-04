5a820ece6a4b5769e6ea4206a9b6c89c8efbf12a
LeeHyunJin <myfamily11141@gmail.com>
2023-08-04 10:58:55 +0900

    [상속] eat() 오버라이딩

--- Inherirance/main.cpp
+++ Inherirance/main.cpp
@@ -7,7 +7,7 @@ using namespace sf;
 class Entity {
 public:
 
-	// 왠만하면 클래스를 매개변ㅅ수로 할 떄 99.99%는 주소값으로 넘기자
+	// 왠만하면 클래스를 매개변수로 할 떄 99.99%는 주소값으로 넘기자
 	// 메모리 용량을 줄일 수 있다. call by value를 피하기 위해
 	Entity(int life, int speed, RectangleShape* sprite)
 		: life_(life), speed_(speed), sprite_(sprite)
@@ -21,7 +21,7 @@ public:
 		sprite_->move(x, y);
 	}
 
-	void eat() {}
+	virtual void eat(Entity* p) {}
 
 	// getter
 	int get_life(void) { return life_; }
@@ -44,6 +44,11 @@ public :
 	Player(int life, int speed, RectangleShape* sprite, int score)
 		: Entity(life, speed, sprite), score_(score)
 	{}
+
+	void eat(Entity* e) override
+	{
+		e->set_life(0);
+	}
 
 private:
 	int score_;
@@ -75,9 +80,9 @@ int main(void)
 	e1.setPosition(rand()%800, rand()%600);
 	e1.setSize(Vector2f(40, 40));
 
-	Entity* player = new Entity(3, 5, &p);
+	Entity* player = new Player(3, 5, &p, 100);
 
-	Entity* enemy1 = new Entity(1, 3, &e1);
+	Entity* enemy1 = new Enemy(1, 3, &e1, 10);
 
 
 	while (window.isOpen())
@@ -91,19 +96,29 @@ int main(void)
 		}
 		
 		int p_speed = player->get_speed();
-		if (Keyboard::isKeyPressed(Keyboard::Right))
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			player->move(p_speed, 0);
-		if (Keyboard::isKeyPressed(Keyboard::Left))
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			player->move(-p_speed, 0);
-		if (Keyboard::isKeyPressed(Keyboard::Up))
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			player->move(0, -p_speed);
-		if (Keyboard::isKeyPressed(Keyboard::Down))
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			player->move(0, p_speed);
+		}
 
+		// player가 enemy와 닿으면
+		if (player->get_sprite().getGlobalBounds().intersects(enemy1->get_sprite().getGlobalBounds()))
+		{
+			player->eat(enemy1);
+		}
 
 		window.clear();
 
-		window.draw(enemy1->get_sprite());
+		if(enemy1->get_life() > 0)
+			window.draw(enemy1->get_sprite());
 		window.draw(player->get_sprite());
 
 		window.display();
