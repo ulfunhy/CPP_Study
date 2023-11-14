6c3d78162c119b83b55895dcfd33968666c4c372
LeeHyunJin <myfamily11141@gmail.com>
2023-11-14 13:47:59 +0900

    static 멤버함수에는 일반 멤버변수가 들어가서는 안된다

--- 2023 CPP/문법/static.cpp
+++ 2023 CPP/문법/static.cpp
@@ -6,13 +6,15 @@ using namespace std;
 class Champ {
 public:
 	Champ(const string& name);
+	// static 멤버함수는 객체 생성없이도 호출가능하다
 	static int get_cnt(void) {
+		non_static = 1;		// static 멤버함수에는 일반 멤버변수가 들어가서는 안된다
 		return cnt_;
 	}
 
 private:
 	static int cnt_;
-
+	int non_static;
 };
 
 // static 멤버변수는 무조건 바깥에서 정의한다(const 같은거 빼고)
